0ae8a82eca04ede6eb4968e61aaf4995d90fa8ef
Hendrik Zwiegelaar <hjzwiegelaar@gmail.com>
2026-05-21 20:17:28 +0200

    Initial project structure creation

---
+++ src/main/java/com/hendrik/javaledgerapi/JavaLedgerApiApplication.java
@@ -0,0 +1,13 @@
+package com.hendrik.javaledgerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLedgerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLedgerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hendrik/javaledgerapi/JavaLedgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendrik.javaledgerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLedgerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
