52e5ee454c5aff5f9d962bdf33384dee22577ff9
Hendrik Zwiegelaar <hjzwiegelaar@gmail.com>
2026-07-14 10:14:49 +0200

    feat: add registration controller endpoint

---
+++ src/main/java/com/hendrik/javaledgerapi/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.hendrik.javaledgerapi.controller;
+
+import com.hendrik.javaledgerapi.dto.request.RegisterRequest;
+import com.hendrik.javaledgerapi.dto.response.UserResponse;
+import com.hendrik.javaledgerapi.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
+        UserResponse response = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
