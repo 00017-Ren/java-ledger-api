ce03fe39f7c1cdaaac4a456f1330a2e11539f547
Hendrik Zwiegelaar <hjzwiegelaar@gmail.com>
2026-07-23 05:10:22 +0200

    feat: wire JWT security configuration

--- src/main/java/com/hendrik/javaledgerapi/config/SecurityConfig.java
+++ src/main/java/com/hendrik/javaledgerapi/config/SecurityConfig.java
@@ -1,17 +1,36 @@
 package com.hendrik.javaledgerapi.config;
 
+import com.hendrik.javaledgerapi.security.JwtAccessDeniedHandler;
+import com.hendrik.javaledgerapi.security.JwtAuthenticationEntryPoint;
+import com.hendrik.javaledgerapi.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler,
+            JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,11 +40,27 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) ->
-                authorize.requestMatchers("/api/v1/auth/**").permitAll().anyRequest().authenticated());
+                authorize.requestMatchers("/api/v1/auth/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
 
         http.csrf(AbstractHttpConfigurer::disable);
 
-        return  http.build();
+        http.sessionManagement((session) -> session.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS
+        ));
+
+        http.exceptionHandling(handler -> handler
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler));
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
     @Bean
