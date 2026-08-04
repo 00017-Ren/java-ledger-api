0eccea07ae89d1077200bbf68e1ca1f87587e893
Hendrik Zwiegelaar <hjzwiegelaar@gmail.com>
2026-08-04 09:46:42 +0200

    feat: add account history GET endpoint with pagination

--- src/main/java/com/hendrik/javaledgerapi/controller/AccountController.java
+++ src/main/java/com/hendrik/javaledgerapi/controller/AccountController.java
@@ -3,10 +3,17 @@ package com.hendrik.javaledgerapi.controller;
 import com.hendrik.javaledgerapi.dto.request.CreateAccountRequest;
 import com.hendrik.javaledgerapi.dto.response.AccountResponse;
 import com.hendrik.javaledgerapi.dto.response.BalanceResponse;
+import com.hendrik.javaledgerapi.dto.response.TransactionResponse;
 import com.hendrik.javaledgerapi.model.Account;
 import com.hendrik.javaledgerapi.security.JwtUserPrincipal;
 import com.hendrik.javaledgerapi.service.AccountService;
+import com.hendrik.javaledgerapi.service.TransactionService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -20,9 +27,11 @@ import java.util.UUID;
 @RequestMapping("/api/v1/accounts")
 public class AccountController {
     private final AccountService accountService;
+    private final TransactionService transactionService;
 
-    public AccountController(AccountService accountService) {
+    public AccountController(AccountService accountService, TransactionService transactionService) {
         this.accountService = accountService;
+        this.transactionService = transactionService;
     }
 
     @PostMapping
@@ -55,6 +64,7 @@ public class AccountController {
             @PathVariable UUID id
     ) {
         AccountResponse response = accountService.getAccountById(id, principal.id());
+
         return ResponseEntity.ok(response);
     }
 
@@ -64,6 +74,19 @@ public class AccountController {
             @PathVariable UUID id
     ) {
         BalanceResponse response = accountService.getAccountBalance(id, principal.id());
+
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}/transactions")
+    public ResponseEntity<PagedModel<TransactionResponse>> getAccountTransactions(
+            @AuthenticationPrincipal JwtUserPrincipal principal,
+            @PathVariable UUID id,
+            @PageableDefault(size = 20,
+                    sort = {"createdAt", "id"},
+                    direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<TransactionResponse> response = transactionService.getAccountHistory(id, principal.id(), pageable);
+
+        return ResponseEntity.ok(new PagedModel<>(response));
     }
 }
